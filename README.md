dbd5bf8f7360d3026e6a9addbf69bfb9dc6f45c2
Chip <chip@example.com>
2025-08-16 19:43:38 -0700

    Initial Commit.

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# bookman
-A program for automatically reading through the Library of Babel looking for novel texts.
+# BookMan
+
+Automatically reads books from Jonathan Basile's online implementation of Jorge Luis Borge's classic short story "The Library of Babel" (http://libraryofbabel.info). Bookman chooses a hex at random and then scans through all the books contained within, looking for evidence of actual, readable English language.
+
+## Features
+- Outputs any findings to an easily parseable SQLite database.
+- Multi-threading support
+- Uses a word-counting heuristic to pre-assess text segments, ensuring that the LLM only gets called when there's a high chance that something's been found.
+- Leverages your own Ollama server (not included) for fast, private local processing.
+- Easy-to-tweak plaintext configuration file.
+
+## Dependencies
+- Access to a working Ollama server: You can use pretty much any language model you want, but it must be running on an Ollama server which you have API access to. I recommend you use a locally-hosted server for optimal performance.
+- Ollama Python module: Can be installed via PIP or whatever your favorite package manager is.
+- SQLite3
+- Soft Requirement: a *nix operating system. By default, BookMan leverages /usr/share/dict/words as its reference dictionary. If you want to run this program on Windows then you'll need to download your own word list and point to it in the config file.
+
+## How to Install
+1. Download this repository
+2. Ensure you have all the dependencies listed above
+3. Open up bookman.conf in a text editor and set the address of your Ollama server (llmHost) as well as your choice of language model (llmModel). While you're there, tweak any other settings that you'd like.
+4. Run bookman.py and enjoy!
+
+## The Configuration File
+Below is an explanation of each of the config file options:
+
+- loglevel : The level of verbosity for the program log. Right now, INFO is the only level supported.
+
+- logfile : The location and name of the log file (default bookman.log).
+
+- dbfile : The location and name of the SQLite database file (default bookman.db).
+
+- testfile : The location and name of the "test book" (default testbook.txt). As the name implies, the "Test Book" is only used when you put the program in a special testing mode - it won't be used during normal operation.
+
+- dictfile : The location and name of the word list to be used as BookMan's reference dictionary. By default, the standard *nix wordlist at /usr/share/dict/words is used. If you are running on Windows or another OS which doesn't use the above, then you need to download your own word list and set its location here. 
+
+- seglength : How long of a text segment (in number of characters) BookMan analyzes at one time. Default is 40.
+
+- wordCount : The minimum number of CONSECUTIVE English words a given text segment needs to have in order to trigger a finding. Default is 5. Protip: I've noticed that if you set it to 3 or less you tend to get a lot of false positives.
+
+- cCount : The total number of processing threads to use (in other words, how many books the program will try to read simultaneously). Set to 0 to have BookMan auto-detect the thread count based on how many logical CPUs your computer has. Default is 0. 
+
+- llmHost : The address and port number of your Ollama server. Default is localhost:11434.
+
+- llmModel : The language model you wish to use to assess candidate text segments. Default is gemma2 just because it's my personal favorite, but you'll need to set it to whatever model you have/prefer.
+
+- llmPrompt : The prompt that's fed to the language model (along with the candidate text segment) to explain to the model what it's being asked to do. I recommend you don't mess with this unless you're very comfortable with prompt engineering.
+
+
+## How to View the Results
+In addition to scrolling log output on the screen (and also bookman.log), BookMan writes its findings to an SQLite3 database. Unfortunately, right now the only way to access the contents of that database is to open it up manually using sqlite's command line utility. I'm working on a proper tool for perusing the database but I haven't written it yet. 
+
+The database contains two tables:
+- stats, which contains a single row of data storing the total number of books read along with your computer's average reading speed (in minutes-per-book).
+- passages, which contains BookMan's actual findings. This includes the exact address of the book in question, the page number the passage was found on, the contents of the passage, its Word Count score, and a boolean flag showing whether or not the LLM confirmed the finding.
+
+## Special Credits
+BookMan wouldn't exist without the following people:
+
+- Jorge Luis Borge, who wrote the original Library of Babel short story.
+
+- Jonathan Basile, who made the Library of Babel a reality via software.
+
+- Victor Barros, who created the "Pybel" API that allows us to download books from the Library. See https://github.com/victor-cortez/Library-of-Babel-Python-API for Victor's original source code.
+
+- Reddit user u/Silly_King3635, who came up with the original idea for BookMan (sorry, don't know their real name!)
---
+++ bookman.py
@@ -0,0 +1,221 @@
+## BOOKMAN
+## by UltraChip
+##
+## Automatically reads books from Jonathan Basile's online implementation of Jorge Luis Borge's
+## classic short story "The Library of Babel" (http://libraryofbabel.info). Bookman chooses a hex
+## at random and then scans through all the books contained within, looking for evidence of actual,
+## readable English language. 
+
+
+# IMPORTS AND CONSTANTS
+import random
+import string
+import sqlite3
+import os
+import time
+import ollama
+import re
+import logging
+import logging.config
+import multiprocessing as mp
+from math import ceil
+
+import pybel
+from configManager import loadConfig
+
+confFile = "./bookman.conf"
+
+
+# FUNCTIONS
+def scanHex(wid):
+    # Select a hex address at random and iterate over each book in that hex, scanning each one for
+    # evidence of language.
+    db = sqlite3.connect(conf['dbfile'])
+    db.execute("PRAGMA isolation_level = SERIALIZABLE")
+
+    dictionary = readDictionary(conf['dictfile'])
+
+    hex = buildAddr()
+    logging.info(f"READER #{wid} - Scanning hex #{shorthex(hex)}")
+    for wall in range(1, 4):
+        for shelf in range(1, 5):
+            for volume in range(1, 32):
+                book = pybel.browse(hex, str(wall), str(shelf), str(volume)).replace("\n", "")
+                #book = testbook()  # NOTE: Only uncomment this line when testing!
+                logging.info(f"READER #{wid} - Book downloaded. Reading...")
+                analyze(book, hex, str(wall), str(shelf), str(volume), db, dictionary, wid)
+    db.close()
+    return
+
+def buildAddr():
+    # Builds a randomized hex address
+    chars = []
+    alphabet = string.ascii_lowercase + string.digits
+    length = random.randint(1, 3200)
+    for c in range(length):
+        chars.append(random.choice(alphabet))
+    return ''.join(chars)
+
+def analyze(book, hex, wall, shelf, volume, db, dictionary, wid):
+    # Analyze the content of a book in conf['seglength'] sized chunks. First counts the number of
+    # consecutive English words in a given segment and, if it surpasses the minimum wordcount,
+    # hands the segment off to an LLM for final verification.
+    i    = 1
+    page = 1
+    cursor = db.cursor()
+    startTime = time.time()
+    while i < (len(book)-conf['seglength']):
+        segment = book[i:(i+conf['seglength'])]
+
+        if (page*3200) < i:
+            page = ceil(i/3200)
+        
+        wc = wCount(segment, dictionary)
+        if wc >= conf['wordCount']:
+            tstamp = time.strftime("%Y%m%d-%H%M%S",time.gmtime())
+            llm = llmCheck(segment)
+            cursor.execute("""INSERT INTO passages (tstamp, hex, wall, shelf, volume, page,
+                            content, nScore, llm) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);""", 
+                            (tstamp, hex, wall, shelf, volume, page, segment, wc, llm))
+            db.commit()
+            i += conf['seglength']
+            if llm == 0:
+                a=0
+                #logging.info(f"FOUND - Segment passed word count check but NO LLM confirmation")
+                #logging.info(f"{shorthex(hex)}-{wall}-{shelf}-{volume}, page {page}:")
+                #logging.info(segment)
+            else:
+                logging.info(f"FOUND - Segment passed ALL checks!!!")
+                logging.info(f"{shorthex(hex)}-{wall}-{shelf}-{volume}, page {page}:")
+                logging.info(segment)
+        else:
+            i += 1
+
+    readTime = (time.time()-startTime)/60  # Calculates reading time in minutes
+    cursor.execute("SELECT books, readSpeed FROM stats WHERE sid = 1;")
+    result = cursor.fetchone()
+    booksRead = int(result[0])
+    oldTotalMinutes = booksRead * result[1]
+    booksRead += 1
+    avgReadSpeed = (oldTotalMinutes+readTime) / booksRead
+    cursor.execute("UPDATE stats SET books = ?, readSpeed = ? WHERE sid = 1", (booksRead, avgReadSpeed))
+    db.commit()
+    return
+
+def wCount(segment, dictionary):
+    # Identifies the longest chain of consecutive English words within the given segment, and
+    # returns it as a numerical score.
+    score    = 0
+    curCount = 0
+    splitSeg = re.split(r'\s*,\s*|\s*\.\s*|\s+', segment)
+
+    for word in splitSeg:
+        if word.strip() in dictionary:
+            curCount += 1
+            if curCount > score:
+                score = curCount
+        else:
+            curCount = 0
+    return score
+
+def llmCheck(segment):
+    # If a given segment is shown to have a high chance of containing language, then we pass it to
+    # an Ollama server and ask it to perform a final confirmation. Returns 1 if the LLM believes
+    # there is language, otherwise returns 0. 
+    while True:
+        thePrompt = f"{conf['llmPrompt']}\n\nThe test string is:\n{segment}"
+        client = ollama.Client(host=conf['llmHost'])
+        answer = client.generate(model=conf['llmModel'], prompt=thePrompt)
+        yesno = answer['response'].strip()
+        if yesno == '1':
+            return 1
+        elif yesno == '0':
+            return 0
+
+def shorthex(hex):
+    # Shortens the hex address to the first 10 characters + last 10 characters to make things more
+    # readable in logs and such.
+    if len(hex) <= 20:
+        return hex
+    firstC = hex[:10]
+    lastC  = hex[-10:]
+    return f"{firstC}...{lastC}"
+
+def initDB(filename):
+    # Initializes the sqlite database which keeps track of found passages
+    if not os.path.exists(filename):
+        db = sqlite3.connect(filename)
+        cursor = db.cursor()
+        tab_passage = """CREATE TABLE passages (
+                         pid INTEGER PRIMARY KEY,
+                         tstamp TEXT,
+                         hex TEXT,
+                         wall TEXT,
+                         shelf TEXT,
+                         volume TEXT,
+                         page INTEGER,
+                         content TEXT,
+                         nScore INTEGER,
+                         llm INTEGER); """
+        tab_stats = """CREATE TABLE stats (
+                       sid INTEGER PRIMARY KEY,
+                       books INTEGER,
+                       readSpeed REAL);"""
+        cursor.execute(tab_passage)
+        cursor.execute(tab_stats)
+        cursor.execute("INSERT INTO stats (books, readSpeed) VALUES (0, 0);")
+        cursor.close()
+        db.commit()
+        db.close()
+    return
+
+def readDictionary(filename):
+    # Reads in the dictionary file and returns it as a list.
+    dictionary = {'a', 'is'}  # Pretty much the only low-character words we want in the list.
+    with open(filename, 'r') as file:
+        for line in file:
+            word = line.strip()
+            if len(word) > 2:  # We want to filter out smaller "words" in the list like dd and tr.
+                word = word.lower()
+                dictionary.add(word.strip())
+    return dictionary
+
+def testbook():
+    # NOTE: THIS FUNCTION IS USED ONLY WHEN TESTING
+    # Loads up the contents of a locally-stored test book (instead of downloading real books from
+    # the Library) with five known-valid English sentences that you can use to test and tune the 
+    # detection algorithms. 
+    with open(conf['testfile'], 'r') as file:
+        book = file.read()
+    return book.replace("\n", "")
+
+
+# INITIALIZATION
+conf = loadConfig(confFile)
+
+logging.config.dictConfig({    # This block silences log spam from imported modules so that only
+    'version': 1,              # my own log messages get recorded.
+    'disable_existing_loggers': True,
+})
+logging.basicConfig(
+    level=conf['loglevel'],
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(conf['logfile'], mode='a'),
+        logging.StreamHandler()])
+logging.info("INIT - Logger")
+
+initDB(conf['dbfile'])
+
+workers = []
+cCount = os.cpu_count() if conf['cCount'] == 0 else conf['cCount']
+mp.get_context("spawn")
+for w in range(cCount):
+    worker = mp.Process(target=scanHex, args=(w,))
+    workers.append(worker)
+    worker.start()
+    logging.info(f"INIT - Kicked off reader #{w}")
+
+logging.info("INIT - All readers online, initialization complete")
+while True:
+    time.sleep(1)  # Just have the main thread idle while the worker threads do their thing.
