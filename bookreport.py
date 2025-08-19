73db8d389e1bc27b6a01ce7e57b5e0aa2ca1040d
Chip <chip@example.com>
2025-08-18 20:52:18 -0700

    WIP commit.

--- bookreport.py
+++ bookreport.py
@@ -8,6 +8,7 @@
 import sqlite3
 import logging
 from prettytable import PrettyTable as pt
+from shutil import get_terminal_size
 
 from configManager import loadConfig
 
@@ -19,7 +20,32 @@ confFile = "./bookman.conf"
 def main():
     # Main menu function
     stats, passages = loadDB(conf['dbfile'])
+    swidth = getWidth()
+
+    clearScreen()
+    print(colSpacer(["BOOKMAN REPORTING UTILITY"], swidth, "="))
     return
+
+def clearScreen():
+    # Self explanatory
+    print("\033c")
+
+def colSpacer(options, swidth, pad=" "):
+    # Evenly spaces strings along the column width of the screen. Useful for
+    # cleanly printing menu options.
+    fString = ""
+    num_options = len(options)
+    if num_options == 1:
+        return options[0]
+    spacePerCol = int(floor((swidth - len(options[-1])) / (num_options - 1)))
+    for i, option in enumerate(options):
+        if i == len(options)-1:
+            break
+        spacer = pad*(spacePerCol-len(option))
+        options[i] = option+spacer
+    for option in options:
+        fString = fString + option
+    return fString
 
 def shorthex(hex):
     # Shortens the hex address to the first 10 characters + last 10 characters to make things more
@@ -49,6 +75,14 @@ def buildAddr(hex, wall, shelf, volume):
     sHex = shorthex(hex)
     addr = f"{sHex}-{wall}-{shelf}-{volume}"
     return addr
+
+def getWidth():
+    # Attempts to detect the width of the terminal (in characters). If auto-detect fails, defaults
+    # to 100.
+    try:
+        return get_terminal_size().columns
+    except:
+        return 100
 
 
 # INITIALIZATION
